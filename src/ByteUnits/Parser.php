ee13855cdb1cddf40a2010c635e2e1d65e231a61
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-08-10 17:11:55 +0200

    Catch ReflectionException

--- src/ByteUnits/Parser.php
+++ src/ByteUnits/Parser.php
@@ -18,19 +18,22 @@ readonly class Parser
 
     /**
      * @throws ParseException
-     * @throws \ReflectionException
      */
     public function parse(string $quantityWithUnit): System
     {
         if (preg_match('/(?P<quantity>[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\W*(?P<unit>.*)/', $quantityWithUnit, $matches)) {
             $quantity = $matches['quantity'];
-            if ($this->scale->isKnownUnit($matches['unit'])) {
-                $unit = $this->scale->normalizeNameOfUnit($matches['unit']);
+            try {
+                if ($this->scale->isKnownUnit($matches['unit'])) {
+                    $unit = $this->scale->normalizeNameOfUnit($matches['unit']);
 
-                return $this->system->newInstanceArgs([$this->scale->scaleFromUnit($quantity, $unit)]);
-            }
-            if (empty($matches['unit'])) {
-                return $this->system->newInstanceArgs([$quantity]);
+                    return $this->system->newInstanceArgs([$this->scale->scaleFromUnit($quantity, $unit)]);
+                }
+                if (empty($matches['unit'])) {
+                    return $this->system->newInstanceArgs([$quantity]);
+                }
+            } catch (\ReflectionException $e) {
+                throw new ParseException($e->getMessage(), $e->getCode(), $e);
             }
         }
         throw new ParseException("'{$quantityWithUnit}' is not a valid byte format");
--- src/ByteUnits/functions.php
+++ src/ByteUnits/functions.php
@@ -19,20 +19,17 @@ function bytes(int|string $numberOf): Metric
     return new Metric($numberOf);
 }
 
-/**
- * @throws ParseException
- */
 function parse(string $bytesAsString): System
 {
-    $lastParseException = null;
     $parsers = [Metric::parser(), Binary::parser()];
     foreach ($parsers as $parser) {
         try {
             return $parser->parse($bytesAsString);
-        } catch (ParseException $lastParseException) {
-        } catch (\Exception $e) {
-            $lastParseException = new ParseException($e->getMessage(), $e->getCode(), $e);
+        } catch (ParseException $e) {
+            $lastParseException = $e;
         }
     }
+
+    /** @var ParseException $lastParseException */
     throw $lastParseException;
 }
